45cb2517fb8a2c683e7e42acda34aaf4bd4650fb
Emil Häggblom <emil.haggblom@htomail.com>
2014-03-17 10:55:39 +0000

    textade till det lite

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var server = http.createServer(function(request, response) {
 			break;
 
 		case 'GET':
-			response.end(items.join(', ') + '\n');
+      response.end('Kom ihåg detta!'items.join(', ') + '\n');
 			break;
 	}
 });
